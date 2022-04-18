d5c52846f8db2dff2b3c9f570d80dfd3c4dadbdc
Dmitrii Verbetsky <dmitrii@openocean.academy>
2022-04-18 23:13:12 +0300

    added react-router

--- src/components/search-bar/search-bar.js
+++ src/components/search-bar/search-bar.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import { useParams, useHistory, useLocation } from 'react-router-dom';
 import './search-bar.css'
 import {DATASET} from '../../moc/dataset'
 
 
 export default function SearchBar() {
-    const [searchTerm, setSearchTerm] = useState('');
+    const {id} = useParams();
+    let history = useHistory();
+
+    const [searchTerm, setSearchTerm] = useState(id);
     const [searchResults, setSearchResults] = useState([]);
 
     const onChangeSearchInput = (event) => {
         setSearchTerm(event.target.value);
+        history.push(`/${event.target.value}`);
     };
 
     useEffect(() => {
@@ -17,6 +22,7 @@ export default function SearchBar() {
         );
         setSearchResults(results);
     }, [searchTerm]);
+
 
     return(
         <div className='search-bar'>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import SearchBar from './components/search-bar';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <SearchBar/>
-  </React.StrictMode>
+  <Router>
+    <Route path="/:id">
+      <SearchBar />
+    </Route>
+  </Router>
 );
